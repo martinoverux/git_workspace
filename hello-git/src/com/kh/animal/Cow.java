8c13ffddbdd731e6fc3e7ac77b32a6415a46b450
martinoverux <veraxanimus@naver.com>
2022-05-09 13:54:20 +0900

    reset/revert 노트 추가

--- hello-git/src/com/kh/animal/Cow.java
+++ hello-git/src/com/kh/animal/Cow.java
@@ -2,6 +2,16 @@ package com.kh.animal;
 
 public class Cow {
 	
+	/**
+	 * reset : 지정 버전으로 돌아감
+	 *   - hard : 이후 변경사항을 모두 제거
+	 *   - mixed : 이후 변경사항을 unstaged changes에서 관리
+	 *   - soft : 이후 변경 사항을 staging area에 추가
+	 *   
+	 * revert : 지정한 버젼의 취소 버젼을 새로 추가
+	 *  - history를 변경할 수 없는 경우 유용하다.
+	 *  - conflict 발생가능성 있음
+	 */
 	private String name;
 	private String type;
 
