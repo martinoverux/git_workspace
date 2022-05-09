ce65426c8fbb9354705ce36ac68ba7fe69f1a59c
martinoverux <veraxanimus@naver.com>
2022-05-09 10:36:37 +0900

    Dog 객체의 bark 기능 호출!

--- hello-git/src/com/kh/git/Helloworld.java
+++ hello-git/src/com/kh/git/Helloworld.java
@@ -1,5 +1,7 @@
 package com.kh.git;
 
+import com.kh.animal.Dog;
+
 public class Helloworld {
 
 	/**
@@ -19,5 +21,8 @@ public class Helloworld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		Dog dog = new Dog();
+		dog.bark();
 	}
 }
